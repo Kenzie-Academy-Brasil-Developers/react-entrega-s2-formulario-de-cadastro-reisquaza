58f3a4c742994253f6b80ebab15c3336dc78e002
Reisquaza <100309835+Reisquaza@users.noreply.github.com>
2022-08-04 20:27:06 -0300

    feat: Pagina de dashboard

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,10 +1,33 @@
-const Dashboard = ({ setAuthorized }) => {
-  const teste = () => {
-    setAuthorized(false);
+import { useEffect } from "react";
+
+const Dashboard = ({ setAuthorized, user, navigate, token }) => {
+  const logout = () => {
+    window.localStorage.clear();
+    navigate("/login");
   };
+
+  useEffect(() => {
+    if (!token) {
+      navigate("/login");
+    }
+  }, [token, navigate]);
+
   return (
     <>
-      <button onClick={() => teste()}>aaaaaaaaaaaaaaaa</button>
+      <header>
+        <h1>Kenzie Hub</h1>
+        <button onClick={() => logout()}>Sair</button>
+      </header>
+      <div>
+        <h2>Olá, {user.name}</h2>
+        <p>{user.course_module}</p>
+      </div>
+      <div>
+        <h2>Que pena! Estamos em desenvolvimento</h2>
+        <h3>
+          Nossa aplicação está em desenvolvimento, em breve teremos novidades
+        </h3>
+      </div>
     </>
   );
 };
