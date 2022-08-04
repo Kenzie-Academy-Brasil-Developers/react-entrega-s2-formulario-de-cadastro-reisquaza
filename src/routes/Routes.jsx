aeac788b037666b0bede05515133dae804fa689f
Reisquaza <100309835+Reisquaza@users.noreply.github.com>
2022-08-04 09:33:48 -0300

    feat: Rotas

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,5 +1,17 @@
+import { Navigate, Route, Routes } from "react-router";
+import Login from "../pages/Login";
+import Profile from "../pages/Profile";
+import Register from "../pages/Register";
+
 const RoutesMain = () => {
-    return (  );
-}
- 
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="*" element={<Navigate replace to={"login"} />} />
+    </Routes>
+  );
+};
+
 export default RoutesMain;
