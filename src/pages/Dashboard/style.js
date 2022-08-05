70c32bca817efbc8cd1ba40e1fa8a9e6be6b7c4e
Reisquaza <100309835+Reisquaza@users.noreply.github.com>
2022-08-05 15:29:26 -0300

    feat: Layout da dashboard

---
+++ src/pages/Dashboard/style.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Header = styled.header`
+  width: 100%;
+  height: 15%;
+  margin-bottom: 20px;
+  border-bottom: 1px solid var(--color-gray-3);
+`;
