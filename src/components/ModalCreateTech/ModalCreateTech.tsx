17077ab16ae9312d8fad1bb4d4ea3171688c36a5
Reisquaza <100309835+Reisquaza@users.noreply.github.com>
2022-08-25 09:53:26 -0300

    fix: Componente form do modalCreatetTech

--- src/components/ModalCreateTech/ModalCreateTech.tsx
+++ src/components/ModalCreateTech/ModalCreateTech.tsx
@@ -1,15 +1,13 @@
 import { useContext } from "react";
-import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import { ModalContext } from "../../contexts/ModalContexts";
 import { TechsContext } from "../../contexts/TechsContexts";
 import { Button } from "../Button";
-import { FormModal } from "../Form";
+import FormTech from "../FormTech";
 import Modal from "../Modal";
 
 const ModalCreateTech = () => {
-  const { createTech }: any = useContext(TechsContext);
+  const { createTech } = useContext<any>(TechsContext);
   const { setIsCreateTech } = useContext(ModalContext);
-  const { register, handleSubmit } = useForm();
 
   const closeModal = () => {
     setIsCreateTech(false);
@@ -21,25 +19,9 @@ const ModalCreateTech = () => {
       title="Cadastrar tecnologia"
       closeModal={closeModal}
     >
-      <FormModal onSubmit={handleSubmit(createTech)}>
-        <label htmlFor="title">
-          <p>Nome</p>
-          <input type="text" id="title" {...register("title")} />
-          {/* <h4>{errors.title?.message}</h4> */}
-        </label>
-
-        <label htmlFor="status">
-          <p>Selecionar status</p>
-          <select id="status" {...register("status")}>
-            <option value="">Status</option>
-            <option value="Iniciante">Iniciante</option>
-            <option value="Avançado">Avançado</option>
-            <option value="Intermediário">Intermediário</option>
-          </select>
-          {/* <h4>{errors.status?.message}</h4> */}
-        </label>
-        <Button></Button>
-      </FormModal>
+      <FormTech techSubmit={createTech} title={"Cadastrar tecnologia"}>
+        <Button>Cadastrar</Button>
+      </FormTech>
     </Modal>
   );
 };
